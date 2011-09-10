5f9e4151ae49eaced4c611f67f4496dc6e44906f
Andrew McGregor <andy@txm.net>
2011-09-10 14:26:15 +0100

    base release

---
+++ txm/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    #('^$', 'django.views.generic.simple.direct_to_template', {'template': 'home.html'}),
+    ('^$', 'txm.views.blog_view'),
+
+    ('^admin/$', 'txm.views.admin_home'),
+    ('^admin/edit/', 'txm.views.admin_edit'),
+    ('^admin/delete/', 'txm.views.admin_delete'),
+)
+
+
---
+++ txm/views.py
@@ -0,0 +1,93 @@
+from django.conf import settings
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+from google.appengine.api import users
+
+from txm.forms import BlogForm
+from txm.models import Blog
+
+
+def blog_view(request):
+
+    blogs = Blog.objects.all()
+    return render_to_response('home.html', { 'blogs': blogs }, context_instance=RequestContext(request))
+
+
+# Borrowed from http://www.factory-h.com/blog/?p=23
+def login_required(fn):
+    """ checks to see if the user is logged in, if not, redirect to login """
+
+    def _dec(view_func):
+        def _checklogin(request, *args, **kwargs):
+
+            user = users.get_current_user()
+            if user:
+                return view_func(request, *args, **kwargs)
+
+            else:
+                return HttpResponseRedirect(users.create_login_url(request.get_full_path()))
+
+        _checklogin.__doc__ = view_func.__doc__
+        _checklogin.__dict__ = view_func.__dict__
+
+        return _checklogin
+
+    return _dec(fn)
+
+
+
+@login_required
+def admin_home(request):
+
+    if request.method == 'POST':
+        form = BlogForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            form = BlogForm()
+
+    else:
+        form = BlogForm()
+
+    blogs = Blog.objects.all()
+
+    return render_to_response('admin_home.html', {
+        'form': form,
+        'blogs': blogs,
+    }, context_instance=RequestContext(request))
+
+
+@login_required
+def admin_edit(request):
+
+    if request.method == 'POST':       # If the form has been submitted...
+        blog = Blog.objects.get(id=request.GET['id'])
+        form = BlogForm(instance=blog,data=request.POST)
+        if form.is_valid():            # All validation rules pass
+            form.save()
+            return HttpResponseRedirect('/admin/') # Redirect after POST
+
+    elif request.GET.has_key('id'):
+        blog = Blog.objects.get(id=request.GET['id'])
+        form = BlogForm(instance=blog)
+        blogs = Blog.objects.all()
+        return render_to_response('admin_home.html', {
+            'form': form,
+            'blogs': blogs,
+            'id': request.GET['id'],
+        }, context_instance=RequestContext(request))
+
+    else:
+        return HttpResponseRedirect('/admin/') # Redirect after POST
+
+
+@login_required
+def admin_delete(request):
+
+    if request.method == 'GET' and request.GET.has_key('id'):
+        blog = Blog.objects.get(id=request.GET['id'])
+        blog.delete()
+
+    return HttpResponseRedirect('/admin/') # Redirect after POST
+
